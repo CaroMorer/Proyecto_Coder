7294bcb97fdeabde94f86f39c5e7d276fe1fee43
CaroMor <morenoamar346@gmail.com>
2022-06-27 00:30:52 -0300

    commit cambio de pc

--- ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/AppCoder/views.py
@@ -1,11 +1,27 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from AppCoder.models import Curso
 # Create your views here.
-def curso(self):
+def curso(request):
     curso = Curso(nombre = "Datos", camada = "22")
     curso.save()
-    documento_texto = f"--Curso: {curso.nombre}, Camada: {curso.camada}"
+    curso2 = Curso(nombre="Marketing", camada="247")
+    curso2.save()
+
+    documento_texto = f"--Curso1: {curso.nombre}, Camada: {curso.camada}, ---Curso2: {curso2.nombre} Camada: {curso2.camada}"
     return HttpResponse(documento_texto)
 
+def inicio(request):
+    return render(request, 'AppCoder/inicio.html')
 
+def cursos(request):
+    return render(request, 'AppCoder/cursos.html')
+
+def profesores(request):
+    return render(request, 'AppCoder/profesores.html')
+
+def estudiantes(request):
+    return render(request, "AppCoder/estudiantes.html")
+
+def entregables(request):
+    return render(request, 'AppCoder/entregables.html')
--- ProyectoCoder/ProyectoCoder/settings.py
+++ ProyectoCoder/ProyectoCoder/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ProyectoCoder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["C:/Users/moren/OneDrive/Documentos/Proyecto Coder/ProyectoCoder/AppCoder/templates/AppCoder"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
